2f80dd1bebb24844162c073d0b347b1b0aa5b5fd
luiel <luielflorez@gmail.com>
2022-10-13 19:43:34 -0500

    Configuracion de Hola Mundo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
       <h1>Hola Mundo</h1>
     </div>
-  );
+  )
 }
 
 export default App;
