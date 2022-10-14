0f5455bda49192b9e22b645138e17e1fbfe58327
luiel <luielflorez@gmail.com>
2022-10-13 22:00:49 -0500

    Organizar lista

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -8,6 +8,18 @@ const Formulario =() => {
         <div className='container mt-5'>
             <h1 className='text-center'>Crud Basico</h1>
             <hr />
+            <div className='row'>
+                <div className="col-8">             
+                    <h4 className="text-center">Listado Frutas</h4>
+                    <ul className="list-group">
+                    </ul>
+                </div>
+
+                <div className='col-4'>
+                <h4 className="text-center">
+                    Agregar Frutas
+                </h4>
+                
             <form>
                 <input 
                 className='form-control mb-2'
@@ -18,6 +30,8 @@ const Formulario =() => {
                 <boton className='btn btn-primary btn-block'>Agregar</boton>
             </form>
 
+            </div>
+            </div>
             </div>
     )
 
